428510ab85aac7f18bf5a5ce2bf759bd6b04168d
Guadoo <rock_jk@msn.com>
2017-07-02 23:53:19 +0800

    Create Chapter3

---
+++ Chapter3/view_data.py
@@ -0,0 +1,20 @@
+import pandas
+import webbrowser
+import os
+
+#Read the dataset into a data table using Pandas
+data_table = pandas.read_csv("ML_Chapter03_House_Dataset.csv")
+
+#Create a web page view of the data for easy viewing
+html = data_table[0:15].to_html()
+
+#Save the html to a temporary file
+with open("data.html", "w") as f:
+    f.write(html)
+
+#Open the web page in our web browser
+full_filename = os.path.abspath("data.html")
+webbrowser.open("file://{}".format(full_filename))
+
+
+
